5c6cc9ae3ec0e38cc88e6941708bc03d492dec47
guiortgon <guiortgon@gmail.com>
2025-11-03 09:59:16 +0100

    initial commit

---
+++ src/main/java/org/srpm/SrpmApplication.java
@@ -0,0 +1,13 @@
+package org.srpm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrpmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SrpmApplication.class, args);
+    }
+
+}
